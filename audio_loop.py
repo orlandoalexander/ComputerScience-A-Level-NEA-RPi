cbd28e74a488804f07d159203c90090d9836fcc3
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-11-26 01:44:50 +0000

    Update audio_loop.py

--- audio_loop.py
+++ audio_loop.py
@@ -1,6 +1,4 @@
-#! /usr/bin/env python
-from subprocess import call
-call(['espeak “Welcome to the world of Robots” 2>/dev/null'], shell=True)
+
 import paho.mqtt.client as mqtt
 import wave
 import pickle
