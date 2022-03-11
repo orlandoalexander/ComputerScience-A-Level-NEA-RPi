2bf3b4bd99a4acf1b6ad150721b0835df0f5d20a
Orlando Alexander <aorlando04@gmail.com>
2022-03-11 04:12:19 +0000

    updates

--- main_pi.py
+++ main_pi.py
@@ -60,6 +60,7 @@ class buttonPressed():
             self.faceBGR = img
             self.faceGray = cv.cvtColor(self.faceBGR, cv.COLOR_BGR2GRAY) # change to gray for opencv
             self.faceRGB = cv.cvtColor(self.faceBGR, cv.COLOR_BGR2RGB) # change to rgb for face_recognition module
+            print(self.faceGray)
             #cv.imshow('gray', self.faceGray)
             #cv.waitKey(0)
             #cv.imshow('bgr', self.faceRGB)
